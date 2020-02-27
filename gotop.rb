9fae35a210733e15b51ce50b6ac5a90db8a70cc5
Sean E. Russell <ser@ser1.net>
2020-02-27 13:19:42 -0600

    Bumped for v3.3.2

--- gotop.rb
+++ gotop.rb
@@ -1,13 +1,13 @@
 class Gotop < Formula
   desc "A terminal based graphical activity monitor inspired by gtop and vtop"
-  homepage "https://github.com/cjbassi/gotop"
-  url "https://github.com/cjbassi/gotop/releases/download/3.0.0/gotop_3.0.0_darwin_amd64.tgz"
-  version "3.0.0"
-  sha256 "8a44d86429f64635c3b3553a149a53592c32fe617511272c6ebcb8ceba2b8134"
+  homepage "https://github.com/xxxserxxx/gotop"
+  url "https://github.com/xxxserxxx/gotop/releases/download/v3.3.2/gotop_3.3.2_darwin_amd64.tgz"
+  sha256 "aaf63438179befa1b19515258910a64317126be9348040298e87162998645d46"
+  version "3.3.2"
 
   if OS.linux?
-    url "https://github.com/cjbassi/gotop/releases/download/3.0.0/gotop_3.0.0_linux_amd64.tgz"
-    sha256 "7fd4c34398a63dcf3189db91ba2dbe9c1d8b96a0a04814ef56b8e764b145e608"
+    url "https://github.com/xxxserxxx/gotop/releases/download/v3.3.2/gotop_3.3.2_linux_amd64.tgz"
+    sha256 "768766ed3a366f48412498314bbe1b341629dde09de18121f787b52d5f52775b"
   end
 
   def install
