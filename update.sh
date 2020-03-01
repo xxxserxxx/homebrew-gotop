993eda66d252c066016229bdedb14cd1e391106b
Sean E. Russell <ser@ser1.net>
2020-03-01 09:18:38 -0600

    Adds a script to update the version.

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Updates the version and checksums of a gotop release
+
+V=$1
+
+for O in linux darwin; do
+	U=https://github.com/xxxserxxx/gotop/releases/download/v${V}/gotop_${V}_${O}_amd64.tgz
+	S=$(curl -sL $U | sha256sum | cut -d ' ' -f 1)
+	# I apologize for this.
+    sed -i "s,\(^ *url \"\)https:.*${O}_amd64.tgz,\1${U},; s/version .*/version \"$V\"/; /${O}_amd64/{n;s/\".*\"/\"$S\"/}" gotop.rb
+done
