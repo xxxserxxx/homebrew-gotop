81207765c36cf7e64fc097be825b06a83de35df6
Tap Updater <ser@ser1.net>
2020-03-06 18:30:56 +0000

    Update recipe to version "v3.5.0"

--- gotop.rb
+++ gotop.rb
@@ -2,12 +2,12 @@ class Gotop < Formula
   version "v3.5.0"
   desc "A terminal based graphical activity monitor inspired by gtop and vtop"
   homepage "https://github.com/xxxserxxx/gotop"
-  url "https://github.com/xxxserxxx/gotop/releases/download/vv3.5.0/gotop_v3.5.0_darwin_amd64.tgz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  url "https://github.com/xxxserxxx/gotop/releases/download/v3.5.0/gotop_v3.5.0_darwin_amd64.tgz"
+  sha256 "caefde24a2b8534bb9a6a05d8b886bcd32216761384026b399da1a713a22070e"
 
   if OS.linux?
-    url "https://github.com/xxxserxxx/gotop/releases/download/vv3.5.0/gotop_v3.5.0_linux_amd64.tgz"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    url "https://github.com/xxxserxxx/gotop/releases/download/v3.5.0/gotop_v3.5.0_linux_amd64.tgz"
+    sha256 "90fd3c4b451f873a009da08b93f8dd908f1236e69adf4d91159e01136d323cd1"
   end
 
   def install
