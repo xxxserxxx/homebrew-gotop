97ec7f24a7ebe10ab437c6c296475cc53d34e2cc
Sean E. Russell <ser@ser1.net>
2020-03-16 13:00:14 -0500

    Trying to fix #80, but homebrew on Linux is not behaving.

--- gotop.rb
+++ gotop.rb
@@ -1,16 +1,17 @@
 class Gotop < Formula
-  version "v3.5.0"
+  version "3.5.0"
   desc "A terminal based graphical activity monitor inspired by gtop and vtop"
   homepage "https://github.com/xxxserxxx/gotop"
-  url "https://github.com/xxxserxxx/gotop/releases/download/v3.5.0/gotop_v3.5.0_darwin_amd64.tgz"
+  url "https://github.com/xxxserxxx/gotop/releases/download/v#{version}/gotop_v#{version}_darwin_amd64.tgz"
   sha256 "caefde24a2b8534bb9a6a05d8b886bcd32216761384026b399da1a713a22070e"
 
   if OS.linux?
-    url "https://github.com/xxxserxxx/gotop/releases/download/v3.5.0/gotop_v3.5.0_linux_amd64.tgz"
+    url "https://github.com/xxxserxxx/gotop/releases/download/v#{version}/gotop_v#{version}_linux_amd64.tgz"
     sha256 "90fd3c4b451f873a009da08b93f8dd908f1236e69adf4d91159e01136d323cd1"
   end
 
   def install
+    mv "gotop_v#{version}_darwin_amd64", "gotop"
     bin.install "gotop"
   end
 end
